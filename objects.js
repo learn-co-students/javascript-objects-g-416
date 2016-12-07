357cc3402f70582dcc80b1842e8512fd04b24130
Jake Brady <jake@cmdrconsulting.com>
2016-12-07 14:25:45 -0700

    done

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'asdf': 'asdg'};
+
+function updatePlaylist(playlist, artist, song){
+  return Object.assign({}, playlist, {[artist]: song});
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
