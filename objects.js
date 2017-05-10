385a36b0ff76d7c8ad091bd20e76badda5a68cc3
andrew <github email address>
2017-05-10 09:25:17 +0300

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist = {
+	Blackmill: 'Miracle',
+	Blackmill: "Anything"
+}
+
+function updatePlaylist(playlist, artist, song) {
+	playlist[artist] = song
+
+  	return playlist
+
+}
+
+function removeFromPlaylist(playlist, artistName) {
+	delete playlist[artistName]
+
+  return playlist
+}
