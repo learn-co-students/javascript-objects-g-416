da050a63be29d27de3be0a8b02cf390b3e136aac
github username <github email address>
2021-02-16 09:46:10 -0800

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
 const playlist=Object.assign({},{artistName:"Beyonce",songTitle:"Formation"})
 
 const updatePlaylist=function(playlist,artistName,songTitle){
-    Object.assign(playlist,{ artistName: songTitle})
+    Object.assign(playlist,{[artistName]: songTitle})
+}
+
+const removeFromPlaylist=function(playlist, artistName){
+    delete playlist[artistName]
+     playlist
 }
